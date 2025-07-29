f68cb0bb2aedf30b4929e56d5421f1b624814cfe
DENNISCSH <denniscristhian4@gmail.com>
2025-07-29 12:32:12 -0500

    Actualización base de datos

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,7 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,       // Obliga a usar SSL
+        rejectUnauthorized: false // Permite certificados autogenerados
+      }
+    },
     logging: false
   }
 );
